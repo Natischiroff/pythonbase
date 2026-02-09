079882b5d20cdc2421aac6cb4c8cc52d723fd339
Natalia Schiroff <natalia.schiroff@escola.pr.gov.br>
2026-02-09 14:15:45 +0000

    aula2

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+num1 = int(input("Digite o primeiro número"))
+num2 = int(input("Digite o segundo número"))
+
+soma = num1+num2
+
+print("A soma é : ",soma)
+
+subtração = num1-num2
+
+print("A subtração é ", subtração)
+
+multiplicação = num1*num2
+
+print("A multiplicação é ", multiplicação)
---
+++ ola.py
@@ -0,0 +1,6 @@
+#nome = input("Digite seu nome:")
+#print("Olá,", nome, "Seja bem vindo")
+
+idade = input("Qual a sua idade? ")
+nome = input("Qual o seu nome? ")
+print("Entendi,sua idade é", idade , "Que legal, E seu nome é", nome)
